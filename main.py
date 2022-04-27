4e8dfaa331dd45d1138823ffa9818de8dbd4a94f
Douglas Bewiahn <dwbewiahn@gmail.com>
2022-04-27 22:13:52 +0100

    Screen Creation

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from tkinter import *
 
+root = Tk()
+root.configure(bg='black')
+root.geometry('1440x720')
+root.title('Mines Sweeper Game - by Douglas Bewiahn')
+root.resizable(False, False)
+root.mainloop()
+
