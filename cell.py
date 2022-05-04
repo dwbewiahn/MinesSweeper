42a839e96edfcca99cfc9ce83e3254738af6ecb6
Douglas Bewiahn <dwbewiahn@gmail.com>
2022-05-05 00:59:33 +0100

    Events for buttons created

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
 from tkinter import Button
 
+
 class Cell:
     def __init__(self, is_mine=False):
         self.is_mine = is_mine
@@ -10,4 +11,14 @@ class Cell:
             location,
             text='Text'
         )
+
+        btn.bind('<Button-1>', self.left_click_actions)
+        btn.bind('<Button-3>', self.right_click_actions)
+
         self.cell_btn_object = btn
+
+    def left_click_actions(self, event):
+        print(event)
+
+    def right_click_actions(self, event):
+        print(event)
