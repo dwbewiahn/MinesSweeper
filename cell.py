8f81d22643c09cfdf19bfa300dd89069e83d7bcb
Douglas Bewiahn <dwbewiahn@gmail.com>
2022-05-06 23:41:53 +0100

    Creating the logic to pick mines random

--- cell.py
+++ cell.py
@@ -1,12 +1,16 @@
 from tkinter import Button
-
+import random
 
 class Cell:
+    all = []
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
         self.x = x
         self.y = y
+
+        Cell.all.append(self) # Append the object to the all list
+
 
     def create_btn_object(self, location):
         btn = Button(
@@ -26,3 +30,13 @@ class Cell:
 
     def right_click_actions(self, event):
         print(event)
+
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(
+            Cell.all, 6
+        )
+        print(picked_cells)
+
+    def __repr__(self):
+        return f"Cell({self.x}, {self.y})"
