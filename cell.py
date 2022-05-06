0d5121d656efdbbf4ddddf5509269dad6a47bb05
Douglas Bewiahn <dwbewiahn@gmail.com>
2022-05-06 23:57:08 +0100

    Mines logic

--- cell.py
+++ cell.py
@@ -19,7 +19,7 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f'{self.x}, {self.y}'
+
         )
 
         btn.bind('<Button-1>', self.left_click_actions)
@@ -28,10 +28,16 @@ class Cell:
         self.cell_btn_object = btn
 
     def left_click_actions(self, event):
-        print(event)
+        if self.is_mine:
+            self.show_mine()
+
+    def show_mine(self):
+        # a logic to game over
+        self.cell_btn_object.configure(bg='red')
 
     def right_click_actions(self, event):
         print(event)
+
 
     @staticmethod
     def randomize_mines():
