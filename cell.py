58dba5d7df33be736889d4369b76888c1eeaf868
Douglas Bewiahn <dwbewiahn@gmail.com>
2022-05-11 23:24:34 +0100

    Test Surround cells to check mines

--- cell.py
+++ cell.py
@@ -5,14 +5,14 @@ import settings
 
 class Cell:
     all = []
+
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
         self.x = x
         self.y = y
 
-        Cell.all.append(self) # Append the object to the all list
-
+        Cell.all.append(self)  # Append the object to the all list
 
     def create_btn_object(self, location):
         btn = Button(
@@ -36,11 +36,35 @@ class Cell:
     def get_cell_by_axis(self, x, y):
         # Return a cell object based on the value of x,y
         for cell in Cell.all:
-            if cell.x ==x and cell.y == y:
+            if cell.x == x and cell.y == y:
                 return cell
 
+    @property
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1),
+        ]
+
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+        return counter
+
     def show_cell(self):
-        pass
+        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
 
     def show_mine(self):
         # a logic to game over
@@ -49,14 +73,13 @@ class Cell:
     def right_click_actions(self, event):
         print(event)
 
-
     @staticmethod
     def randomize_mines():
         picked_cells = random.sample(
             Cell.all, settings.MINES_COUNT
         )
         for picked_cell in picked_cells:
-           picked_cell.is_mine = True
+            picked_cell.is_mine = True
 
     def __repr__(self):
         return f"Cell({self.x}, {self.y})"
